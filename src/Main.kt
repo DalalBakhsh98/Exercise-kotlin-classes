1fdde49aeae40890c4ef02b0a5dd67641809d295
Dalal Bakhsh <DalalBakhsh98@gmail.com>
2025-02-16 17:29:39 +0300

    The first part

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,15 @@
 fun main() {
-    println("Hello World!")
+
+    var myBook = Book()
+
+    myBook.author = "Alexandre Dumas"
+    myBook.title = "The black Tulip"
+    myBook.yearPublished = 1850
+}
+
+class Book(){
+    var title: String = ""
+    var author: String = ""
+    var yearPublished: Int = 0
+
 }
