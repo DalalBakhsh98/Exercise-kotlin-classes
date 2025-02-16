d82afed6e8ba129a08af1cc036f339901e65180b
Dalal Bakhsh <DalalBakhsh98@gmail.com>
2025-02-16 18:25:49 +0300

    All done with bonus

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,24 @@
 fun main() {
 
-    var myBook = Book()
+    var myBook = Book("The black Tulip","Alexandre Dumas", 1850, "Novel")
 
+    println(myBook.getBookInfo())
+    /* part 2
     myBook.author = "Alexandre Dumas"
     myBook.title = "The black Tulip"
     myBook.yearPublished = 1850
+     */
 }
 
-class Book(){
+class Book(var title: String, var author: String, var yearPublished: Int = 0, var genre: String){
+
+    /* part 1
     var title: String = ""
     var author: String = ""
     var yearPublished: Int = 0
+    */
 
+    fun getBookInfo(){
+        println("$title by $author ($yearPublished) - Genre: $genre")
+    }
 }
